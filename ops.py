61399e0e3a7024c8ad46cbc49d8c4a599a881063
Dang Thi <dangthi09703@gmail.com>
2023-11-24 11:13:39 +0700

    add

--- ops.py
+++ ops.py
@@ -28,3 +28,23 @@ class Div:
         g_y = - g * x.value / (y.value * y.value)
         return [g_x, g_y]
     
+class Sin:
+    def forward(x):
+        return np.sin(x.value)
+    
+    def backward(g, x, y):
+        return [g * np.cos(x.value)]
+
+class Cos:
+    def forward(x):
+        return np.cos(x.value)
+    
+    def backward(g, x, y):
+        return [-g * np.sin(x.value)]
+
+class Tan:
+    def forward(x):
+        return np.tan(x.value)
+    
+    def backward(g, x, y):
+        return [g * (1 / np.cos(x.value))**2]
